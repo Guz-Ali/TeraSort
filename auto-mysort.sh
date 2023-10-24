ec23cf9c89d2539ebcde4cba142223c585f322e8
Guz-Ali <aliguzelyel@gmail.com>
2023-10-24 11:52:54 +0000

    auto-mysort.sh finished

--- auto-mysort.sh
+++ auto-mysort.sh
@@ -1,7 +1,7 @@
 for num_threads in 1 4 8 16
 do
-	#psrecord "./mysort 1GB.txt sort.txt $num_threads >> mysort1GB.log" --log "mysort1GB-CPU-MEM.txt" --interval 1
-	psrecord "./mysort 1gb.txt sort.txt $num_threads >> mysort1GB.log" --log "./psrecord_analysis/mysort1GB-$num_threads-threads_log.txt" --interval 1 --include-children --plot "./psrecord_analysis/mysort1GB-$num_threads-threads_log.png"
+	psrecord "./mysort 1GB.txt sort.txt $num_threads >> mysort1GB.log" --log "./psrecord_analysis/mysort1GB-$num_threads-threads_log.txt" --interval 1 --include-children --plot "./psrecord_analysis/mysort1GB-$num_threads-threads_log.png"
+	echo "mysort 1GB $num_threads finished."
 	#./valsort sort.txt >> mysort1GB.log
 	rm -rf sort.txt
 	find  . -name 'tmp*' -exec rm {} \;
@@ -9,8 +9,8 @@ done
 
 for num_threads in 1 4 8 16
 do
-	psrecord "./mysort 4gb.txt sort.txt $num_threads >> mysort4GB.log" --log "./psrecord_analysis/mysort4GB-$num_threads-threads_log.txt" --interval 1 --include-children --plot "./psrecord_analysis/mysort4GB-$num_threads-threads_log.png"
-	#./mysort 4GB.txt sort.txt $num_threads >> mysort4GB.log
+	psrecord "./mysort 4GB.txt sort.txt $num_threads >> mysort4GB.log" --log "./psrecord_analysis/mysort4GB-$num_threads-threads_log.txt" --interval 1 --include-children --plot "./psrecord_analysis/mysort4GB-$num_threads-threads_log.png"
+	echo "mysort 4GB $num_threads finished."
 	#./valsort sort.txt >> mysort4GB.log
 	rm -rf sort.txt
 	find  . -name 'tmp*' -exec rm {} \;
@@ -18,8 +18,8 @@ done
 
 for num_threads in 4 8 24
 do
-	psrecord "./mysort 16gb.txt sort.txt $num_threads >> mysort16GB.log" --log "./psrecord_analysis/mysort16GB-$num_threads-threads_log.txt" --interval 1 --include-children --plot "./psrecord_analysis/mysort16GB-$num_threads-threads_log.png"
-	#./mysort 16GB.txt sort.txt $num_threads >> mysort16GB.log
+	psrecord "./mysort 16GB.txt sort.txt $num_threads >> mysort16GB.log" --log "./psrecord_analysis/mysort16GB-$num_threads-threads_log.txt" --interval 1 --include-children --plot "./psrecord_analysis/mysort16GB-$num_threads-threads_log.png"
+	echo "mysort 16GB $num_threads finished."
 	#./valsort sort.txt >> mysort16GB.log
 	rm -rf sort.txt
 	find  . -name 'tmp*' -exec rm {} \;
@@ -27,8 +27,8 @@ done
 
 for num_threads in 4 8 16 24
 do
-	psrecord "./mysort 64gb.txt sort.txt $num_threads >> mysort64GB.log" --log "./psrecord_analysis/mysort64GB-$num_threads-threads_log.txt" --interval 1 --include-children --plot "./psrecord_analysis/mysort64GB-$num_threads-threads_log.png"
-	#./mysort 64GB.txt sort.txt $num_threads >> mysort64GB.log
+	psrecord "./mysort 64GB.txt sort.txt $num_threads >> mysort64GB.log" --log "./psrecord_analysis/mysort64GB-$num_threads-threads_log.txt" --interval 1 --include-children --plot "./psrecord_analysis/mysort64GB-$num_threads-threads_log.png"
+	echo "mysort 64GB $num_threads finished."
 	#./valsort sort.txt >> mysort64GB.log
 	rm -rf sort.txt
 	find  . -name 'tmp*' -exec rm {} \;
